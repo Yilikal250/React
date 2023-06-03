128ce4ac69d864d722ae49f8d2be31be570e202c
Yilikal250 <teshomeyilikal250@gmail.com>
2023-06-03 11:21:16 +0300

    react set up added

---
+++ react-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function Greetings() {
+
+    return <h4>This is the first component
+    </h4>;
+}
+
+ReactDOM.render(<Greetings/>,document.getElementById('root'));
