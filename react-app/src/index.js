407895f8d337257f23ddc0ec889593f7b869ea56
Yilikal250 <teshomeyilikal250@gmail.com>
2023-06-05 12:23:52 +0300

    book new added as an object

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,35 +1,40 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const firstBook ={
+const books =[
+  {
   img:'https://m.media-amazon.com/images/I/51uCRF3qUUL._AC._SR180,230.jpg',
   author:'The Bone Spindle',
-  title:'School Trip: A Graphic Novel'
-}
-const secondBook ={
+  title:'School Trip: A Graphic Novel',
+},
+{
   img:'https://images-eu.ssl-images-amazon.com/images/I/81QVYq-iPkS._AC_UL160_SR160,160_.jpg',
   author:'by Christophe Alexis Perez',
-  title:'School Trip: A Graphic Novel'
-}
-
+  title:'School Trip: A Graphic Novel',
+},
+];
+const names = ['john','peter','susan'];
+const newNames=names.map((names)=>{
+ return <h1>{names}</h1>;
+});
+console.log(newNames);
 function BookList() {
     return (
-      <section className='booklist'>
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
-    </section>
+      <section className='booklist'>{newNames}</section>
     );
 }
 
 const Book =(props) => {
- console.log(props)
+  const {img,title,author,children} = props;
+  console.log(props);
 return(
   <article>
-    <img src={props.img} alt=''/>
-    <h1>{props.title}</h1>
-    <h4>{props.author}</h4>
-
+    <img src={img} alt=''/>
+    <h1>{title}</h1>
+    {children}
+    <h4>{author}</h4>
+   
   </article>
   ); 
 };
